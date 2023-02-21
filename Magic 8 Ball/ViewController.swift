f99ad4d4e67d67640515b44feb0dae91869a9b4c
JuanNeiverth <polli.neiverth@gmail.com>
2023-02-20 20:39:59 -0600

    Meu primeiro desafio finalizado do curso de Swift da Udemy

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var imageVIew: UIImageView!
+    
+    @IBAction func askButton(_ sender: UIButton) {
+       
+        let ballArray = [
+        UIImage(named: "ball1.png"),
+        UIImage(named: "ball2.png"),
+        UIImage(named: "ball3.png"),
+        UIImage(named: "ball4.png"),
+        UIImage(named: "ball5.png")
+        ]
+        
+        imageVIew.image = ballArray[Int.random(in: 0...4)]
+        
+        
+    }
+    
+    
+    
+ 
 
 
 
